5963e7c2cdb6696d2f47d0b551c2575cb23a1892
Melecito <ahimelecc123@gmail.com>
2022-05-09 20:27:42 -0500

    Cambio de nombre de pos a Look y cuidado ademas base de datos del proyect personal

---
+++ script/look_y_cuidado.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2022 a las 03:12:07
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 7.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `look_y_cuidado`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `IdCitas` int(11) NOT NULL,
+  `CitaFecha` date DEFAULT NULL,
+  `Color` varchar(50) NOT NULL,
+  `CitHora` time DEFAULT NULL,
+  `IdMedicoV` int(11) NOT NULL,
+  `IdCliente` int(11) NOT NULL,
+  `IdMascota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `IdCliente` int(11) NOT NULL,
+  `Cedula` varchar(15) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
+  `Edad` int(11) NOT NULL,
+  `Celular` varchar(20) NOT NULL,
+  `Genero` enum('m','f') NOT NULL,
+  `Correo` varchar(50) NOT NULL,
+  `IdMedicoV` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `IdCompra` int(11) NOT NULL,
+  `FechaComp` date DEFAULT NULL,
+  `IdCliente` int(11) NOT NULL,
+  `CodProduc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `IdMascota` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Color` varchar(50) NOT NULL,
+  `Edad` int(11) NOT NULL,
+  `Raza` varchar(20) NOT NULL,
+  `Genero` enum('m','f') NOT NULL,
+  `IdMedicoV` int(11) NOT NULL,
+  `IdCliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicoveterinario`
+--
+
+CREATE TABLE `medicoveterinario` (
+  `IdMedicoV` int(11) NOT NULL,
+  `Celular` varchar(20) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
+  `Especialidad` varchar(50) NOT NULL,
+  `Consultorio` char(3) NOT NULL,
+  `Genero` enum('m','f') NOT NULL,
+  `Correo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `CodProduc` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Cantidad` varchar(50) NOT NULL,
+  `Precio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `usuario` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `password` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `perfil` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `foto` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `estado` int(11) NOT NULL,
+  `ultimo_login` datetime NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
