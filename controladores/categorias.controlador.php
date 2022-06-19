f4ace40919c16f2d6042bc76b998d671571cde58
Melecito <ahimelecc123@gmail.com>
2022-06-19 18:34:51 -0500

    Agregar categorias ok

--- controladores/categorias.controlador.php
+++ controladores/categorias.controlador.php
@@ -11,6 +11,35 @@ class ControladorCategorias(){
 				
 
 
+			}else{
+
+				echo '<script>
+
+
+					swal({
+
+						type: "error",
+						title: "¡La categoria no puede ir vacía o llevar caracteres especiales!",
+						showConfirmButton: true,
+						confirmButtonText: "Cerrar",
+						closeOnConfirm: false
+
+						}).then((result)=>{
+
+							if(result.value){
+
+
+								window.location = "categorias";
+
+
+							}
+
+
+							});
+
+
+
+				</script>';
 			}
 
 		}
